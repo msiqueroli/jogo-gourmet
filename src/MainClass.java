7bbb7895df5762619c6bf7a38fc4b8e45ce3660e
Marcos Vinicius Siqueroli <msiqueroli@gmail.com>
2019-11-30 14:07:31 -0300

    Removendo bibliotecas não utilizadas!

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,14 +1,7 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 public class MainClass extends Frame {
-
     public static void main(String[] args) {
-
-        Janela janela = new Janela();
-
-
+        new Janela();
     }
 }
